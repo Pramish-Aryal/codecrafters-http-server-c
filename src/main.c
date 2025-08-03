2ebbc6e040fbf166132fc4597e275d4e9b744b49
Pramish Aryal <prar@logpoint.com>
2025-08-03 01:44:56 +0000

    fix: change starts_with to sq for matching '/'

--- src/main.c
+++ src/main.c
@@ -83,7 +83,7 @@ int main()
 
     String return_buffer;
     StringView echo = sv_from_cstr("/echo/");
-    if (sv_starts_with(request_target, sv_from_cstr("/"))) {
+    if (sv_eq(request_target, sv_from_cstr("/"))) {
         StringView response_200 = sv_from_cstr("HTTP/1.1 200 OK\r\n\r\n");
         return_buffer = sv_to_owned(arena, response_200);
     } else if (sv_starts_with(request_target, echo)) {
