8ddf064561b3a2ad264414af1c45f63cf9353533
Pramish Aryal <prar@logpoint.com>
2025-08-10 10:03:44 +0000

    fix: routes with '/' are handled properly

--- src/main.c
+++ src/main.c
@@ -131,124 +131,6 @@ bool parse_http_request(Arena* arena, HttpRequest* request, StringView* http_req
     }
 }
 
-// void* handle_socket_thread(void* arg)
-// {
-//     int client_fd = *(int*)arg;
-//
-//     Arena arena_stack = { 0 };
-//     Arena* arena = &arena_stack;
-//     u32 backing_buffer_len = MB(1);
-//     char* backing_buffer = malloc(backing_buffer_len);
-//     arena_init(arena, backing_buffer, backing_buffer_len);
-//
-//     u32 client_buffer_size = 1024;
-//     char* client_buffer = arena_alloc(arena, client_buffer_size);
-//     int bytes_read = read(client_fd, client_buffer, client_buffer_size);
-//
-//     // assume bytes_read < client_buffer_size -1;
-//     client_buffer[bytes_read] = 0; // null terminate (though its unnecessary as arena_alloc returns a null terminated buffer)
-//     StringView http_request = sv_from_cstr(client_buffer);
-//     StringView iter = http_request;
-//
-//     // format to parse: GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n
-//     StringView request_line = sv_chop_by_delim(&iter, '\n');
-//     printf("request-line: " SV_Fmt "\n", SV_Arg(request_line));
-//     StringView http_method = sv_chop_by_delim(&request_line, ' '); // GET
-//     StringView request_target = sv_chop_by_delim(&request_line, ' '); // /index.html
-//     StringView http_version = sv_chop_by_delim(&request_line, ' '); // HTTP/1.1
-//     assert(sv_trim(request_line).len == 0 && "request line must have been fully parsed");
-//
-//     HashTable headers = { 0 };
-//     hash_table_init(arena, &headers);
-//     StringView header_line = sv_trim(sv_chop_by_delim(&iter, '\n'));
-//     while (header_line.len != 0) {
-//         StringView header_key = sv_chop_by_delim(&header_line, ':');
-//         StringView header_value = sv_trim(header_line);
-//         String lower_case_key = sv_to_owned(arena, header_key);
-//         string_to_lower(lower_case_key);
-//         hash_table_set(&headers, string_to_sv(lower_case_key), sv_to_owned(arena, header_value).data);
-//         header_line = sv_trim(sv_chop_by_delim(&iter, '\n'));
-//     }
-//
-//     printf("\nhttp_method: " SV_Fmt "\nrequest_target:" SV_Fmt "\nhttp_version: " SV_Fmt "\n\n", SV_Arg(http_method), SV_Arg(request_target), SV_Arg(http_version));
-//
-//     String return_buffer;
-//     StringView echo = sv_from_cstr("/echo/");
-//     StringView files = sv_from_cstr("/files/");
-//     if (sv_eq(request_target, sv_from_cstr("/"))) {
-//         StringView response_200 = sv_from_cstr("HTTP/1.1 200 OK\r\n\r\n");
-//         return_buffer = sv_to_owned(arena, response_200);
-//     } else if (sv_starts_with(request_target, files)) {
-//         if (global_directory_path == NULL) {
-//             StringView response_404 = sv_from_cstr("HTTP/1.1 404 Not Found\r\n\r\n");
-//             return_buffer = sv_to_owned(arena, response_404);
-//         } else {
-//             StringView files_ = sv_chop_left(&request_target, files.len);
-//             assert(sv_eq(files, files_));
-//             return_buffer = (String) {
-//                 .data = arena_alloc(arena, KB(256)),
-//                 .capacity = KB(256),
-//                 .len = 0,
-//             };
-//
-//             StringView target_file = sv_trim(request_target);
-//             String file_path = (String) {
-//                 .data = arena_alloc(arena, 256),
-//                 .capacity = 256,
-//             };
-//             string_concat(&file_path, sv_from_cstr(global_directory_path));
-//             string_concat(&file_path, target_file); // assume file_path is null terminated because file_path is originally filled with zero bytes
-//
-//             if (sv_eq(http_method, sv_from_cstr("GET"))) {
-//                 String file = read_entire_file_arena(arena, file_path.data);
-//                 if (file.len == 0) {
-//                     StringView response_404 = sv_from_cstr("HTTP/1.1 404 Not Found\r\n\r\n");
-//                     return_buffer = sv_to_owned(arena, response_404);
-//                 } else {
-//                     return_buffer.len = snprintf(return_buffer.data, return_buffer.capacity, "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n" SV_Fmt, (int)file.len, SV_Arg(file));
-//                 }
-//             } else if (sv_eq(http_method, sv_from_cstr("POST"))) {
-//                 StringView content_length_str = sv_from_cstr(hash_table_get(&headers, sv_from_cstr("content-length")));
-//                 int content_length = sv_chop_u64(&content_length_str);
-//                 StringView content = sv_from_parts(iter.data, content_length);
-//                 write_entire_file(file_path.data, content);
-//                 StringView response_201 = sv_from_cstr("HTTP/1.1 201 Created\r\n\r\n");
-//                 return_buffer = sv_to_owned(arena, response_201);
-//             } else {
-//                 StringView response_400 = sv_from_cstr("HTTP/1.1 400 Bad Request\r\n\r\n");
-//                 return_buffer = sv_to_owned(arena, response_400);
-//             }
-//         }
-//     } else if (sv_eq(request_target, sv_from_cstr("/user-agent"))) {
-//         const char* user_agent_cstr = hash_table_get(&headers, sv_from_cstr("user-agent"));
-//         StringView user_agent = sv_from_cstr(user_agent_cstr);
-//         return_buffer = (String) {
-//             .data = arena_alloc(arena, 1024),
-//             .capacity = 1024,
-//         };
-//         return_buffer.len = snprintf(return_buffer.data, return_buffer.capacity, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n" SV_Fmt, (int)user_agent.len, SV_Arg(user_agent));
-//     } else if (sv_starts_with(request_target, echo)) {
-//         StringView echo_ = sv_chop_left(&request_target, echo.len);
-//         assert(sv_eq(echo, echo_));
-//         return_buffer = (String) {
-//             .data = arena_alloc(arena, 1024),
-//             .capacity = 1024,
-//         };
-//
-//         return_buffer.len = snprintf(return_buffer.data, return_buffer.capacity, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n" SV_Fmt, (int)request_target.len, SV_Arg(request_target));
-//     } else {
-//         StringView response_404 = sv_from_cstr("HTTP/1.1 404 Not Found\r\n\r\n");
-//         return_buffer = sv_to_owned(arena, response_404);
-//     }
-//
-//     write(client_fd, return_buffer.data, return_buffer.len);
-//
-//     close(client_fd);
-//     free(backing_buffer);
-//
-//     return NULL;
-// }
-
 typedef void HandlerFunc(Arena*, HttpRequest*, HttpResponse*);
 
 typedef struct RouteMapping {
@@ -385,6 +267,14 @@ headers_done:
 
         // /echo/:echo_path/:some_other_path/fixed_path // mapping path
         // /echo/path_to_echo/other_path/fixed_path // route path
+
+        // if the path exactly matches, there is no need to do this I guess?
+        if (sv_eq(route_sv, mapping_route)) {
+            // TODO: maybe validate if mapping route doesn't have any route params defined
+            selected_mapping = mapping;
+            mapping_found = true;
+            break;
+        }
 
         Temp_Arena_Memory temp_arena = temp_arena_memory_begin(arena);
 
